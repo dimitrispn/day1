eb22fbecf6cbd8d4cfe96fb1829af62e9f6a1178
dimitrispn <dimitrios@tutamail.com>
2024-10-22 23:06:04 +0300

    day1

---
+++ main.js
@@ -0,0 +1,50 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+
+/*
+comments can make code readable, easy to reuse and informative 
+ */
+
+let name = "dimitris"
+
+Boolean (name = "dimitris" )
+
+
+
+let age 
+console.log(age);
+
+let empty = null
+console.log(empty);
+
+let nickName = "giorgos"
+console.log(typeof(nickName));
+
+let number = 5
+console.log(typeof(number));
+
+let compare = 10>2
+console.log(typeof(compare));
+
+let fruit 
+console.log(typeof(fruit));
+
+let theEmpty = null
+console.log(typeof(theEmpty));
+
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+let variable1 = 1
+let variable1 = 2
+let variable1 = 3
+let variable1 = 4
+
+
+let firstName = "Menios"
+let lastName = "Papadopoulos"
+let maritalStatus = "Single"
+let country = "Greece"
+let age = 30
